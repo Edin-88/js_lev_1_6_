0a5e066b2100c1cfefed82330fbcc18c544a1fba
Your Name <selmanovic.edin1@gmail.com>
2023-01-03 13:59:56 +0100

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// let x = 34
+// x = 67
+
+// console.log(x);
+
+const y = 34
+y = 67
+console.log(y);
+
+// Eine mit const deklarierte Variable kann nicht noch einmal vergeben werden
+// auch wenn diese eine anderen Wert zugewiesen bekommt. 
